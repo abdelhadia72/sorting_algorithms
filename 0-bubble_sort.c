a4c1e8a2cc985782685357ae20fe81987689ead4
abdelhadia72 <thisisavnb@gmail.com>
2023-10-20 16:37:07 +0100

    Fixing Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,10 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp;
-	int swip = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		int swip = 0;
 
 		for (j = 0; j < size - i - 1; j++)
 		{
