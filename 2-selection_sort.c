9049acd0c979088d44e38b86a2f6a0d351106c1f
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-22 16:32:09 +0200

    sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,6 @@
 #include "sort.h"
 
 /**
- * sortArray - sort an array using selection sort
- * @arr: array to sort
- * @idx: index
- * Return: void
-*/
-
-void sortArray(int arr[], int idx)
-{
-        int pivot = arr[idx];
-        int iter = idx - 1;
-        while (iter >= 0 && arr[iter] > pivot)
-        {
-                arr[iter + 1] = arr[iter];
-                iter--;
-        }
-        arr[iter + 1] = pivot;
-}
-
-/**
  * selection_sort - sorting an array using selection sort
  * @array: an array to sort
  * @size: size of the array
@@ -28,11 +9,23 @@ void sortArray(int arr[], int idx)
 
 void selection_sort(int *array, size_t size)
 {
-        size_t idx = 1;
-        while (idx < size)
+        size_t i, j;
+
+        for (i = 0; i < size - 1; i++)
         {
-                sortArray(array, idx);
-                print_array(array, size);
-                idx++;
+                size_t min_idx = i;
+                for (j = i + 1; j < size; j++)
+                {
+                if (array[j] < array[min_idx])
+                        min_idx = j;
+                }
+
+                if (min_idx != i)
+                {
+                        int temp = array[i];
+                        array[i] = array[min_idx];
+                        array[min_idx] = temp;
+                        print_array(array, size);
+                }
         }
 }
